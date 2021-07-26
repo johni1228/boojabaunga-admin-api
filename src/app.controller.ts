466f6166a03b969319b0b3d5823717cf3b494c3c
johni1228 <akirus1228@gmail.com>
2021-07-26 15:40:20 -0700

    fex: generate jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport'
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(LocalAuthGuard)
+
+  @Post('auth/login')
+  async login(@Request() req) {
+    return  req.user;
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
 }
