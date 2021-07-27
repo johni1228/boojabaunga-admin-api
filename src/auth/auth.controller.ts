79388a4e93968d1813900ec985d74220651e0b3e
johni1228 <akirus1228@gmail.com>
2021-07-27 10:24:07 -0700

    fea: add jwt.strategy.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authSerivce: AuthService) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('auth/login')
+    async login(@Request() req){
+        return this.authSerivce.login(req.user); // return jwt created with username, userId 
+    }
+}
