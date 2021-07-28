54f81dcd09a9c37128da1d4d02f8e1074c98ab8b
johni1228 <akirus1228@gmail.com>
2021-07-28 06:28:49 -0700

    fea: add swagger module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,8 +5,7 @@ import { UsersService } from '../users/users.service';
 @Injectable()
 export class AuthService {
     constructor(private usersService: UsersService,
-                private jwtService: JwtService
-    ){}
+                private jwtService: JwtService){}
 
     async vaildateUser(username: string, password: string) : Promise<any> {
         const user = await this.usersService.findOne(username);
